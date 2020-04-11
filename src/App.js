3d4d1947130691b6ee3fa1daadbfa13dbe59b667
Markese Straughter <Kese08@hotmail.com>
2020-04-11 12:47:02 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';// useState is a hook imported from react...A hook allows you to hook into a react feature
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount]= useState(0)// Variable is count..setCount changes variable
+  let [likes, setLike]= useState('Likes')
+  function handleCount () {
+    setCount(count + 1)
+    setLike(likes)
+    if (count === 0){
+      setLike('Like')
+    }
+    else if (count === 1){
+        setLike('Likes')
+    }
+  console.log(likes)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='myButton'>
+            <button onClick={handleCount}>{count} {likes}</button>
     </div>
+    
   );
 }
 
 export default App;
+
+
